eed99cc78e0d16f9189e891def3e7c752fdea45f
akshit8282 <akshitbatra222@gmail.com>
2021-11-07 13:41:13 +0530

    modify

--- src/Components/signOut.js/signOut.js
+++ src/Components/signOut.js/signOut.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+
 
 const signOut = props => {
   if (localStorage.getItem('userTokenTime')) {
     localStorage.removeItem('userTokenTime');
   }
-  return <Redirect to="/" />;
+  return window.location.replace('/');
 }
 
 export default signOut;
